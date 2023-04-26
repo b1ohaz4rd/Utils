b796a805006d181cf67df794ddedd4be221246d6
xZyn <122235431+7BioHazard@users.noreply.github.com>
2023-04-26 18:38:12 +0200

    Create Drawing.lua

---
+++ Drawing.lua
@@ -0,0 +1,37 @@
+local Drawing = {}
+Drawing.__index = Drawing
+
+function Drawing.New()
+    local self = setmetatable({}, Drawing)
+    self.Cache = {}
+
+    return self
+end
+
+function Drawing:Add(ID, Type, Props)
+    local Item = Drawing.new(Type)
+    for i, v in next, Props do
+        Item[i] = v
+    end
+    self.Cache[ID] = Item
+    return Item
+end
+
+function Drawing:Delete(ID)
+    self.Cache[ID]:Remove()
+    self.Cache[ID] = nil
+end
+
+function Drawing:ClearAll()
+    for _, Cache in pairs(self.Cache) do
+        Cache:Remove()
+    end
+end
+
+function Drawing:Destroy()
+    self:ClearAll()
+
+    setmetatable(self, nil)
+end
+
+return Drawing
